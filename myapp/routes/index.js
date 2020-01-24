ce324ddd7ef10c93eace06b63e4dc1b33e7db244
“babbleman” <keizaburo.takashiba@force-corp.co.jp>
2020-01-24 20:43:52 +0900

    k

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var pool=require('pg-pool');
+const { Client } = require('pg');
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+client.connect();
 var b=require('../classfiles/board.js')
 /* GET home page. */
 router.get('/othello', function(req, res, next) {
@@ -12,16 +17,23 @@ router.get('/login', function(req, res, next) {
 router.get('/', function(req, res, next) {
   res.render('top');
 });
-router.get('/db',async(req,res)=>{
-  try{
-    const client=await pool.connect()
-    const result=await client. query('select * from users');
-    const results={'results':(result)? result.rows: null}
-    res.render('/db',results);
-  }catch(err){
-    console.error(err);
-    res.send("Error"+err)
+// router.get('/db',async(req,res)=>{
+//   try{
+//     const client=await pool.connect()
+//     const result=await client. query('select * from users');
+//     const results={'results':(result)? result.rows: null}
+//     res.render('/db',results);
+//   }catch(err){
+//     console.error(err);
+//     res.send("Error"+err)
+//   }
+// })
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
   }
-})
+  client.end();
+});
 
 module.exports = router;
