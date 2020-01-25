e7c9873557262c0aaf22ac2fd1bff2674325d62b
“babbleman” <keizaburo.takashiba@force-corp.co.jp>
2020-01-25 13:17:24 +0900

    K

--- myapp/classfiles/boardのコピー.js
+++ myapp/classfiles/boardのコピー.js
@@ -1,14 +1,22 @@
-let {PythonShell} = require('python-shell')
-var x="./python/test.py"
-var pyshell=new PythonShell(x)
-pyshell.send(20)
-pyshell.on('message',function(data,err){
-  // if (err) throw err
-  console.log(data);
-})
+// let {PythonShell} = require('python-shell')
+// var x="./python/test.py"
+// var pyshell=new PythonShell(x)
+// pyshell.send(20)
+// pyshell.on('message',function(data,err){
+//   // if (err) throw err
+// //   console.log(data);
+// })
 // PythonShell.run(x, null, function (err,data) {
 //   if (err) throw err;
 //   for(var i=0;i<10;i++){
 //     console.log(data[i]);
 //   }
 // });
+client.query('SELECT * from users;', (err, result) => {
+  if (err) throw err;
+  res.render('db',{dd:result})
+  for (let row of result.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
