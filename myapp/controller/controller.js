3d398b46c6b71f2284d3985e7ab088b1bbdcac07
“babbleman” <keizaburo.takashiba@force-corp.co.jp>
2020-01-27 00:54:05 +0900

    m

--- myapp/controller/controller.js
+++ myapp/controller/controller.js
@@ -71,7 +71,7 @@ module.exports={
         console.log("新しいクライアント");
         // console.log(client._queryable);
         // console.log(client._connecting);
-        client.query('insert into users values(name=$1,password=$2);',qstr, (err, result) => {
+        client.query('insert into users values($1,$2);',qstr, (err, result) => {
                 if (err) throw err;
         console.log("登録に成功しました");
         res.redirect('/top')
